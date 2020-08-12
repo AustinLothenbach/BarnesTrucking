4e084e95a6bdc02333e7509e73535d81f1a48ff8
AustinLothenbach <austin.lothenbach@smail.rassmusen.edu>
2020-08-12 16:16:15 -0500

    Updates

---
+++ precache-manifest.184e36ea59cb1a12a8c7a9e05d652871.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ac3cf47e8ee08850dd26b24c1129ac3",
+    "url": "/BarnesTrucking/index.html"
+  },
+  {
+    "revision": "efaa04228d53d995eb4d",
+    "url": "/BarnesTrucking/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "c7fc7fbd6fe09bfc9101",
+    "url": "/BarnesTrucking/static/css/main.d76eb53f.chunk.css"
+  },
+  {
+    "revision": "efaa04228d53d995eb4d",
+    "url": "/BarnesTrucking/static/js/2.a883c30a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/BarnesTrucking/static/js/2.a883c30a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7fc7fbd6fe09bfc9101",
+    "url": "/BarnesTrucking/static/js/main.11efc7c7.chunk.js"
+  },
+  {
+    "revision": "b33d7c201e4656003800",
+    "url": "/BarnesTrucking/static/js/runtime-main.f042a0e8.js"
+  },
+  {
+    "revision": "0128ad85f80191cbabd9dd2df55a2ec2",
+    "url": "/BarnesTrucking/static/media/landing1.0128ad85.jpg"
+  },
+  {
+    "revision": "6573d8d4022bd2840cc1804e42fa5823",
+    "url": "/BarnesTrucking/static/media/landing2.6573d8d4.jpg"
+  },
+  {
+    "revision": "c6b601e2eac859fc160d924956b1bca5",
+    "url": "/BarnesTrucking/static/media/landing3.c6b601e2.jpg"
+  },
+  {
+    "revision": "74eca823ac3b17f1e597fc6cc59f0718",
+    "url": "/BarnesTrucking/static/media/landing4.74eca823.jpg"
+  }
+]);
